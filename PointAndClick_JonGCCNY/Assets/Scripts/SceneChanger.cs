e1f467ed8d0083dac5975812103ab1b0476034b6
jawn1890 <jawn1890@gmail.com>
2024-03-11 18:11:26 -0400

    we created 3 different scenes and explored one way to change scenes (but we will learn more wednesday)

---
+++ PointAndClick_JonGCCNY/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Use Scene Management Library
+
+public class SceneChanger : MonoBehaviour
+{
+
+// CCNY MW IN CLASS
+
+//GLOBAL VARIABLES
+public  int sceneNumber;
+    //0 = StartScene
+    //1 = MainScene
+    //2 = EndScene
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneNumber == 0) //we're in the start scene
+        {
+            StartSceneControls();
+        } else if (sceneNumber == 1) //we're in the main scene
+        {
+            MainSceneControls();
+        } else if (sceneNumber == 2) //we're in the end scene
+        {
+            EndSceneControls();
+        }
+    }
+
+    public void StartSceneControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("MainScene");
+            //Debug.Log("Return Pressed");
+        }
+    }
+
+    public void MainSceneControls()
+    {
+        //code is the same as StartSceneControls, just with different keys
+        //Debug.Log("main scene controls");
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+    public void EndSceneControls()
+    {
+        //code is the same as MainSceneControls, just with different keys
+        //Debug.Log("end scene controls");
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+
+
+}
