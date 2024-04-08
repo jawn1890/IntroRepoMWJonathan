64b552715de1e2e94495ec94a08646343c888f85
jawn1890 <jawn1890@gmail.com>
2024-04-08 17:08:54 -0400

    added new script

---
+++ 2DPlatformer_JonGarcia_CCNY/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    //IN CLASS CCNY MW
+    //GLOBAL VARIABLES
+
+    public int damage;
+    public PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
