e46112763197a26bcb6de3378def0131b37abc49
jawn1890 <jawn1890@gmail.com>
2024-05-06 23:51:18 -0400

    Added a button and a scene changer to the game!

--- 2DPlatformer_JonGarcia_CCNY/Assets/Scripts/SceneChanger.cs
+++ 2DPlatformer_JonGarcia_CCNY/Assets/Scripts/SceneChanger.cs
@@ -6,12 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     //GLOBAL VARIABLES
-    public bool gameOver;
-    public int sceneNumber;
-        //0 = StartScene
-        //1 = MainScene
-        //2 = EndScene
-public PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,56 +16,12 @@ public PlayerController playerControllerScript;
     // Update is called once per frame
     void Update()
     {
-        if (sceneNumber == 0)
-        {
-            StartSceneControls();
-        }
-        else if (sceneNumber == 1)
-        {
-            MainSceneControls();
-        }
-        else if (sceneNumber == 2)
-        {
-            EndSceneControls();
-        }
-    }
-
-    public void StartSceneControls()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //First we're gonna debug; just checking to see if the spacebar press is registered.
-            SceneManager.LoadScene("MainScene");
-            //Debug.Log("Spacebar Pressed");
-
-        }
-    }
-
-    public void MainSceneControls()
-    {
-        gameOver = playerControllerScript.gameOver;
-
-        if (gameOver)
-        {
-            //Debugging to check if this script is being called when falling off a platform.
-            //Debug.Log("Game Over!");
-            SceneManager.LoadScene("EndScene");
-            
-        }
         
     }
 
-    public void EndSceneControls()
+    public void MoveToScene(int sceneID)
     {
-        //Debug.Log("End Scene");
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-
-            //Checking that the return key is pressed.
-            //Debug.Log("Enter Key Pressed");
-            gameOver = false;
-            SceneManager.LoadScene("StartScene");
-        }
+        SceneManager.LoadScene(sceneID);
     }
+
 }
